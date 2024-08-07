7aa4ce7572cd8d581c51bdb5342756dd42cba4ed
Victor <coolstorybrotherz@gmail.com>
2024-08-07 12:31:53 +0200

    .

--- src/app/components/movies.js
+++ src/app/components/movies.js
@@ -1,36 +1,34 @@
-// "use client";
-// import Image from "next/image";
-// import { useState, useEffect } from "react";
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
 
-// export default function Movie() {
-//     const [movieList, setMovieList] = useState([]);
+export default function Movie() {
+    const [movieList, setMovieList] = useState([]);
 
-//     useEffect(() => {
-//         const getData = async () => {
-//             const query = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=5c9ee24f78ba392f2a88ae9d3073cd7f");
-//             const response = await query.json();
-//             console.log(`console log :`, response);
-//             setMovieList(response.results);
-//         };
-//         getData();
-//     }, []);
+    useEffect(() => {
+        const getData = async () => {
+            const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+            const query = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`);
+            const response = await query.json();
+            console.log(`console log :`, response);
+            setMovieList(response.results);
+        };
+        getData();
+    }, []);
 
-//     return (
-//         <div>
-//             <h1>MOVIES API</h1>
-//             <div className="flex flex-wrap gap-5 justify-around">
-//                 {movieList.map((movie) => (
-//                     <div key={movie.id} className="movie-card">
-//                         <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} width={200} height={300} alt={movie.title} />
-//                         <h2>{movie.title}</h2>
-//                         <p>Release Date: {movie.release_date}</p>
-//                         <p>Rating: {movie.vote_average}</p>
-//                     </div>
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// }
-
-// poster_path
-// title
+    return (
+        <div>
+            <h1>MOVIES API</h1>
+            <div className="flex flex-wrap gap-5 justify-around">
+                {movieList.map((movie) => (
+                    <div key={movie.id} className="movie-card">
+                        <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} width={200} height={300} alt={movie.title} />
+                        <h2>{movie.title}</h2>
+                        <p>Release Date: {movie.release_date}</p>
+                        <p>Rating: {movie.vote_average}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
