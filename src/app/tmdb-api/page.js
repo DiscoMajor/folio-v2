38a85754bc8971aea655e3dac14309cb4e1da211
Victor <coolstorybrotherz@gmail.com>
2024-08-09 16:19:33 +0200

    .

--- src/app/tmdb-api/page.js
+++ src/app/tmdb-api/page.js
@@ -5,6 +5,27 @@ import { useState, useEffect } from "react";
 export default function Movie() {
     const [discoverMovieList, setdiscoverMovieList] = useState([]);
     const [UpcomingMoviesList, setUpcomingMoviesList] = useState([]);
+    const [genresList, setGenresList] = useState([]);
+
+    useEffect(() => {
+        const getGenres = async () => {
+            const apiKey = process.env.NEXT_PUBLIC_TMDB;
+            const request = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`);
+            const res = await request.json();
+            setGenresList(res.genres);
+            console.log(`GENRE :`, res);
+        };
+        getGenres();
+    }, []);
+
+    const getGenreNames = (genreIds) => {
+        return genreIds
+            .map((id) => {
+                const genre = genresList.find((g) => g.id === id);
+                return genre ? genre.name : null;
+            })
+            .filter(Boolean);
+    };
 
     useEffect(() => {
         const getUpcoming = async () => {
@@ -52,7 +73,7 @@ export default function Movie() {
 
             <section className="mx-10 py-10">
                 <h1 className="text-2xl p-10 font-bold text-center">Upcoming Section</h1>
-                <div className="grid justify-items-center gap-5 xs:gap-5 sm:gap-8 md:gap-10 lg:gap-14 xl:gap-14 xxl:gap-14 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xxl:grid-cols-4 justify-center">
+                <div className="grid justify-items-center xs:gap-5 sm:gap-8 md:gap-10 xxl:gap-14 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 xxl:grid-cols-4 justify-center">
                     {UpcomingMoviesList.slice(3, 11).map((UpcomingMovie) => (
                         <div key={UpcomingMovie.id} className="flex flex-col gap-2">
                             <div
@@ -63,10 +84,8 @@ export default function Movie() {
                                     backgroundPosition: "center",
                                 }}
                             >
-                                <div className="flex flex-col justify-center items-center absolute bottom-0 w-full h-24 bg-slate-700 bg-opacity-50 p-2 rounded-t-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
-                                    <p className="text-sm font-semibold">Genre ;</p>
-                                    <p className="text-sm font-semibold">Durée ?</p>
-                                    <p className="text-sm font-semibold">Pays ?</p>
+                                <div className="flex flex-col justify-center items-center absolute bottom-0 w-full h-20 bg-slate-700 bg-opacity-50 p-2 rounded-t-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+                                    <p>{getGenreNames(UpcomingMovie.genre_ids).join(", ")}</p>
                                 </div>
 
                                 <div className="text-sm bg-slate-300 bg-opacity-50 rounded p-2 absolute top-3 left-3 ">
@@ -74,9 +93,9 @@ export default function Movie() {
                                 </div>
                             </div>
 
-                            <div className="flex flex-col gap-3 items-center">
-                                <h2 className="text-base font-semibold max-w-72 text-center">{UpcomingMovie.title}</h2>
-                                <p className="text-sm text-gray-400">Release Date: {UpcomingMovie.release_date}</p>
+                            <div className="flex flex-col gap-3 items-center max-w-72">
+                                <h2 className="text-base font-semibold  text-center">{UpcomingMovie.title}</h2>
+                                <p className="text-sm text-gray-400">Date de sortie : {UpcomingMovie.release_date}</p>
                             </div>
                         </div>
                     ))}
