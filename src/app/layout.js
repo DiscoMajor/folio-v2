8230fcd47ba152e41c366b1e7d20230dcf4412ab
Victor <coolstorybrotherz@gmail.com>
2024-08-21 10:25:21 +0200

    Added Analytics from vercel app.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
     title: "Victor's PortFolio",
@@ -9,6 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" className="overflow-y-scroll no-scrollbar scroll-smooth">
+            <Analytics />
             <body className="bg-black">{children}</body>
         </html>
     );
