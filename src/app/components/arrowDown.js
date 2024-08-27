1a28579b6955cc506b48042bc89b37a7e10a28be
Victor <coolstorybrotherz@gmail.com>
2024-08-27 14:31:12 +0200

    Minor fix

--- src/app/components/arrowDown.js
+++ src/app/components/arrowDown.js
@@ -3,7 +3,7 @@ import ArrowDown from "/public/icons/arrowdown.svg";
 
 export default function DownArrow() {
     return (
-        <div className="mx-auto arrow-down lg:p-12 xs:p-5">
+        <div className="mx-auto arrow-down lg:p-10 xs:p-5">
             <Image src={ArrowDown} alt="ArrowDown" />
         </div>
     );
