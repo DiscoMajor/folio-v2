9c3262c07d6fc512bc1b472de7333c00f9eef3e1
Victor <coolstorybrotherz@gmail.com>
2024-08-28 09:15:36 +0200

    .

--- src/app/projets/tmdb-api/components/discover-movies-carousel.js
+++ src/app/projets/tmdb-api/components/discover-movies-carousel.js
@@ -55,7 +55,7 @@ const Carousel = () => {
     };
 
     if (discoverMovieList.length === 0 || genresList.length === 0) {
-        return <p className={`text-white text-xl font-semibold flex justify-center`}>Chargement des données...</p>;
+        return <p className={`text-white text-2xl font-semibold flex justify-center items-center h-screen`}>Chargement des données...</p>;
     }
 
     return (
