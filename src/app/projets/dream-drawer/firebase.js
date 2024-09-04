73abf9f30ffa93b9c35c51820148a6dec8003c8f
Victor <coolstorybrotherz@gmail.com>
2024-09-04 15:08:20 +0200

    .

--- src/app/projets/dream-drawer/firebase.js
+++ src/app/projets/dream-drawer/firebase.js
@@ -1,17 +1,17 @@
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-//     authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_PROJECTID,
-//     storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-//     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
-//     appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
-//     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
-// };
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
