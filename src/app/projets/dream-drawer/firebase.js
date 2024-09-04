bb536f47fc8212fc8a30576cfec9ad41f7f925ac
Victor <coolstorybrotherz@gmail.com>
2024-09-04 13:59:33 +0200

    .

--- src/app/projets/dream-drawer/firebase.js
+++ src/app/projets/dream-drawer/firebase.js
@@ -1,19 +1,15 @@
-// // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // import { getAuth } from "firebase/auth";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 // const firebaseConfig = {
 //     apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
 //     authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-//     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-//     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-//     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+//     projectId: process.env.NEXT_PUBLIC_PROJECTID,
+//     storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+//     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+//     appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+//     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
 // };
 
 // // Initialize Firebase
